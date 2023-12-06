648a61f691af7bfe1f55107db3481f598190fe32
Max Gruebmeyer <max@gruebmeyer.de>
2023-12-06 16:52:44 +0100

    Added first draft of minesweeper map

---
+++ src/mines.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+#include "mines.h"
+
+#define FIELD_SIZE 3
+#define EXPLOSION_PROBABILITY 4
+
+/*
+ * Field structure:
+ * - 5 Bits for counting explosives in neighboring mines
+ * - 1 Bit for open/closed (1/0)
+ * - 1 Bit for flag yes/no (1/0)
+ * - 1 Bit for explosive yes/no (1/0)
+ */
+uint8_t map[FIELD_SIZE][FIELD_SIZE];
+
+const uint8_t OPEN_BIT = 1 << 2;
+const uint8_t FLAG_BIT = 1 << 1;
+const uint8_t EXPLOSIVE_BIT = 1;
+
+static uint8_t *get_field(const uint8_t x, const uint8_t y)
+{
+    if (x >= FIELD_SIZE || y >= FIELD_SIZE) {
+        /* TODO (GM): Proper error handling! */
+        return NULL;
+    }
+
+    return &(map[x][y]);
+}
+
+static uint8_t generate_explosive_bit() {
+    return (rand() % EXPLOSION_PROBABILITY) == 0 ? EXPLOSIVE_BIT : 1;
+}
+
+void init()
+{
+    for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int j = 0; j < FIELD_SIZE; j++) {
+            map[i][j] = generate_explosive_bit();
+        }
+    }
+
+    /* TODO (GM): Set explosive bits! */
+}
+
+void flag(const uint8_t x, const uint8_t y)
+{
+    /* TODO (GM): Does this work? */
+    /* TODO (GM): How to unit test in C? */
+    uint8_t *field = get_field(x, y);
+    *field = *field | FLAG_BIT;
+}
+
+void unflag(const uint8_t x, const uint8_t y)
+{
+    uint8_t *field = get_field(x, y);
+    *field = *field & (~FLAG_BIT);
+}
+
+uint8_t open(const uint8_t x, const uint8_t y)
+{
+    uint8_t *field = get_field(x, y);
+    *field = *field | OPEN_BIT;
+
+    return *field & EXPLOSIVE_BIT;
+}
---
+++ src/mines.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+void init();
+void flag(const uint8_t x, const uint8_t y);
+void unflag(const uint8_t x, const uint8_t y);
+uint8_t open(const uint8_t x, const uint8_t y);

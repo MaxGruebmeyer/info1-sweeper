e69e991ac145aa5eba67323890cce3ae874c2eeb
Max Gruebmeyer <max.gruebmeyer@corpuls.com>
2023-12-08 08:45:26 +0100

    Changed compile.sh so it builds to src/bin

--- src/compile.sh
+++ src/compile.sh
@@ -5,23 +5,19 @@ cflags="-Wall -Wextra -ansi -pedantic"
 
 dir=$(dirname ${BASH_SOURCE[0]})
 original=$(pwd)
+out='bin'
 
 cd $dir
+mkdir -p $out
 
-files=($(ls -A ./*.c))
-arr_len="${#files[@]}"
-
-compile_cmd="gcc $cflags -o a.out"
-for (( i=0; i<$arr_len; i++ )); do
-    file=${files[$i]}
-    obj_file=$(echo $file | sed -r 's/\.c$/\.o/')
-
+compile_cmd="gcc $cflags -o $out/a.out"
+for file in *.c; do
+    obj_file=$(echo "$out/$file" | sed -r 's/\.c$/\.o/')
     gcc $cflags $file -o $obj_file -c
+
     compile_cmd="$compile_cmd $obj_file"
 done
 
-# TODO (GM): Remove
-echo $compile_cmd
 eval $compile_cmd
 
 cd $original

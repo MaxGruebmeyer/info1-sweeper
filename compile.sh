39e4140764fd66099175d0619269cf2582bf5e1c
Max Gruebmeyer <max.gruebmeyer@corpuls.com>
2023-12-08 08:51:47 +0100

    Moved compile.sh from src to root folder, adjusted script and .gitignore accordingly

--- compile.sh
+++ compile.sh
@@ -11,8 +11,8 @@ cd $dir
 mkdir -p $out
 
 compile_cmd="gcc $cflags -o $out/a.out"
-for file in *.c; do
-    obj_file=$(echo "$out/$file" | sed -r 's/\.c$/\.o/')
+for file in src/*.c; do
+    obj_file=$(echo $file | sed -r 's/\.c$/\.o/' | sed "s/src/$out/")
     gcc $cflags $file -o $obj_file -c
 
     compile_cmd="$compile_cmd $obj_file"

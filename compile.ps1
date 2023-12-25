2678acaa450006aead5bfe98c01fce951c8ce662
Max Gruebmeyer <max@gruebmeyer.de>
2023-12-25 16:44:41 +0000

    Switched to build-shell in compile.ps1 and removed obsolete build-windows-git-bash from Taskfile

--- compile.ps1
+++ compile.ps1
@@ -1,5 +1,4 @@
-# Replace this with calling compile.sh?
 $proc = Start-Process -NoNewWindow -PassThru -FilePath "$env:Programfiles\Git\git-bash.exe" `
-	-ArgumentList "-c", "'task build-windows-git-bash; sleep 3s'"
+	-ArgumentList "-c", "'task build-shell; sleep 3s'"
 
 $proc.WaitForExit()

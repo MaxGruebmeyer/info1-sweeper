a59477d90eeaa726a5aa2fc24e2477bc0540483c
Max Gruebmeyer <max@gruebmeyer.de>
2023-12-25 20:08:19 +0100

    Added include guards to mines.h
    Signed-off-by: Max Gruebmeyer <max@gruebmeyer.de>

--- src/mines.h
+++ src/mines.h
@@ -1,6 +1,11 @@
+#ifndef MINES_H
+#define MINES_H
+
 #include <stdint.h>
 
 void init();
 void flag(const uint8_t x, const uint8_t y);
 void unflag(const uint8_t x, const uint8_t y);
 uint8_t open(const uint8_t x, const uint8_t y);
+
+#endif

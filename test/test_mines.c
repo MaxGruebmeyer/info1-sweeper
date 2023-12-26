d120c6dc3538e7e583eac55658def686f9bdc73b
Max Gruebmeyer <max@gruebmeyer.de>
2023-12-26 22:24:44 +0100

    Replaced test_mines.c with a dummy test (there were no real tests there yet anyway)
    Signed-off-by: Max Gruebmeyer <max@gruebmeyer.de>

--- test/test_mines.c
+++ test/test_mines.c
@@ -1,68 +1,22 @@
 #include <stdlib.h>
 
-/* TODO (GM): Remove */
-#include <stdio.h>
-
 #include "../src/mines.h"
 #include "Unity/src/unity.h"
 
-#define MAXIMUM_X 4
-#define MAXIMUM_Y 5
-
-uint8_t **test_map;
-
 void setUp(void)
 {
-    int i;
-
-    /* TODO (GM): Test this with an array later as well! */
-    test_map = malloc(MAXIMUM_X);
-    TEST_ASSERT_NOT_NULL(test_map);
-
-    for (i = 0; i < MAXIMUM_X; i++) {
-        test_map[i] = malloc(MAXIMUM_Y);
-        TEST_ASSERT_NOT_NULL(test_map[i]);
-    }
-
-    init(test_map, MAXIMUM_X, MAXIMUM_Y);
+    /* TODO (GM): Setup! */
 }
 
 void tearDown(void)
 {
-    int i = 0;
-    for (; i < MAXIMUM_X; i++) {
-        printf("%i: %p\n", i, test_map[i]);
-
-        /* TODO (GM): Why does this result in a double free? */
-        /* free(test_map[i]); */
-        test_map[i] = NULL;
-    }
-
-    printf("Last one!\n");
-
-    /* TODO (GM): Why does this free lead to an exit code 127 on windows? */
-    /* free(test_map); */
-
-    test_map = NULL;
-    printf("After Last one!\n");
+    /* TODO (GM): Teardown! */
 }
 
 /* TODO (GM): Replace this with a real test later */
 void bullshit_test(void)
 {
-    int i = 0;
-    for ( ; i < MAXIMUM_X; i++) {
-        int j = 0;
-        for ( ; j < MAXIMUM_Y; j++) {
-            uint8_t explosive = 0;
-            open(&explosive, i, j);
-
-            /* TODO (GM): Replace this with real assertions! */
-            if (explosive) {
-                TEST_ASSERT_EQUAL(1, explosive);
-            }
-        }
-    }
+    TEST_ASSERT_EQUAL(2, 1 + 1);
 }
 
 int main(void)
